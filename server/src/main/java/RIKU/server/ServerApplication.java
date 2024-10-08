f2c58acb0450b10ebc6d4b9af681c0b299f65061
arkchive <kyungminkim7@konkuk.ac.kr>
2024-10-08 13:30:42 +0900

    프로젝트 생성 및 yml 파일 생성

---
+++ server/src/main/java/RIKU/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package RIKU.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
