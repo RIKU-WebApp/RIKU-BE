ab1238079920031b4227df880c62443abfc468bf
arkchive <kyungminkim7@konkuk.ac.kr>
2024-11-06 15:18:14 +0900

    jwt gradle, yml 및 exception 추가

--- src/main/java/RIKU/server/Util/BaseResponseStatus.java
+++ src/main/java/RIKU/server/Util/BaseResponseStatus.java
@@ -17,12 +17,28 @@ public enum BaseResponseStatus {
      */
     BAD_REQUEST(false, 2000, "잘못된 요청입니다."),
 
+    // JWT 예외 - Filter에서 처리
+    ACCESS_DENIED(false, 2001, "권한이 없는 유저의 접근입니다."),
+    EMPTY_AUTHORIZATION_HEADER(false, 2002, "Authorization 헤더가 존재하지 않습니다."),
+    EXPIRED_ACCESS_TOKEN(false, 2003, "이미 만료된 Access 토큰입니다."),
+    UNSUPPORTED_TOKEN_TYPE(false, 2004, "지원되지 않는 토큰 형식입니다."),
+    MALFORMED_TOKEN_TYPE(false, 2005, "인증 토큰이 올바르게 구성되지 않았습니다."),
+    INVALID_SIGNATURE_JWT(false, 2006, "인증 시그니처가 올바르지 않습니다"),
+    INVALID_TOKEN_TYPE(false, 2007, "잘못된 토큰입니다."),
+
+    // Refresh Token 예외 - Exception Handler에서 처리
+    EXPIRED_REFRESH_TOKEN(false, 2008, "Refresh 토큰이 만료되어 재로그인이 필요합니다."),
+    INVALID_REFRESH_TOKEN(false, 2009, "잘못된 Refresh 토큰입니다."),
+
+    // IdToken 예외
+    EXPIRED_ID_TOKEN(false, 2010, "이미 만료된 ID 토큰입니다."),
+
     // UserException
-    INVALID_USER_SIGNUP(false, 2001, "회원가입 요청에서 잘못된 값이 존재합니다."),
-    DUPLICATED_STUDENTID(false, 2002, "중복된 아이디입니다."),
-    USER_NOT_FOUND(false, 2003, "존재하지 않는 회원입니다."),
-    INVALID_PASSWORD(false, 2004, "비밀번호가 일치하지 않습니다."),
-    INVALID_USER_LOGIN(false, 2005, "로그인 요청에서 잘못된 값이 존재합니다."),
+    INVALID_USER_SIGNUP(false, 2011, "회원가입 요청에서 잘못된 값이 존재합니다."),
+    DUPLICATED_STUDENTID(false, 2012, "중복된 아이디입니다."),
+    USER_NOT_FOUND(false, 2013, "존재하지 않는 회원입니다."),
+    INVALID_PASSWORD(false, 2014, "비밀번호가 일치하지 않습니다."),
+    INVALID_USER_LOGIN(false, 2015, "로그인 요청에서 잘못된 값이 존재합니다."),
 
 
     /**
