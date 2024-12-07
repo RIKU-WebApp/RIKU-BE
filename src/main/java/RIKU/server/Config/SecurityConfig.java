7d157a8f6a1390b7ef14625b1cb12d768ef1e059
arkchive <kyungminkim7@konkuk.ac.kr>
2024-12-07 22:16:12 +0900

    feat: cors url 추가

--- src/main/java/RIKU/server/Config/SecurityConfig.java
+++ src/main/java/RIKU/server/Config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://localhost:5173", "http://localhost:5174", "https://localhost:5174"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("location", "Authorization"));
