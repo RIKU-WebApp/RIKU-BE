30b0f0d27bf7ad8ac04e4b0a327a863d51c2c580
arkchive <kyungminkim7@konkuk.ac.kr>
2024-12-09 23:09:06 +0900

    feat: CORS 설정 프론트 배포 주소 추가

--- src/main/java/RIKU/server/Config/SecurityConfig.java
+++ src/main/java/RIKU/server/Config/SecurityConfig.java
@@ -71,7 +71,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://localhost:5173", "http://localhost:5174", "https://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173/", "https://localhost:5173/",
+                                                "http://localhost:5174/", "https://localhost:5174/",
+                                                "https://www.riku.run/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("location", "Authorization"));
