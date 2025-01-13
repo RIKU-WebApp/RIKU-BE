4b8aa2fa83e6070aac1b6b6ecb43e3444fe7efd7
arkchive <kyungminkim7@konkuk.ac.kr>
2025-01-14 01:21:50 +0900

    feat: 마이페이지 조회 포인트 및 활동내역 필드 추가

--- src/main/java/RIKU/server/Dto/User/Response/ReadUserProfileResponseDto.java
+++ src/main/java/RIKU/server/Dto/User/Response/ReadUserProfileResponseDto.java
@@ -16,6 +16,8 @@ public class ReadUserProfileResponseDto {
     private String userName;
     private String userProfileImg;
     private UserRole userRole;
+    private int points = 0;
+    private int attendanceCount = 0;
 
     public static ReadUserProfileResponseDto of(User user) {
         return ReadUserProfileResponseDto.builder()
@@ -23,6 +25,8 @@ public class ReadUserProfileResponseDto {
                 .userName(user.getName())
                 .userProfileImg(user.getProfileImageUrl())
                 .userRole(user.getUserRole())
+                .points(0)
+                .attendanceCount(0)
                 .build();
     }
 
