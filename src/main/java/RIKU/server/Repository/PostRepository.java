9a9d253124bbb9622a215f71b3413c7ce7fc207e
arkchive <kyungminkim7@konkuk.ac.kr>
2025-01-14 00:25:03 +0900

    refactor: jpa 쿼리 추가

--- src/main/java/RIKU/server/Repository/PostRepository.java
+++ src/main/java/RIKU/server/Repository/PostRepository.java
@@ -14,9 +14,11 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     // 번개런 게시글 조회
+    @Query("SELECT p FROM FlashPost p")
     List<FlashPost> findAllFlashPosts();
 
     // 정규런 게시글 조회
+    @Query("SELECT p FROM RegularPost p")
     List<RegularPost> findAllRegularPosts();
 
     // 해당 날짜 게시글 조회
