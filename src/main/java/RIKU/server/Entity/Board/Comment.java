3a7f4b7ba5b63e598d55ea429a4a27564d2d27de
arkchive <kyungminkim7@konkuk.ac.kr>
2025-01-15 17:36:30 +0900

    feat: Comment 엔티티 추가

---
+++ src/main/java/RIKU/server/Entity/Board/Comment.java
@@ -0,0 +1,50 @@
+package RIKU.server.Entity.Board;
+
+import RIKU.server.Entity.BaseEntity;
+import RIKU.server.Entity.BaseStatus;
+import RIKU.server.Entity.User.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "Comments")
+@Getter
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(columnDefinition = "Text")
+    private String content;
+
+    @Column(name = "target_id")
+    private Long targetId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "base_status")
+    private BaseStatus baseStatus = BaseStatus.ACTIVE;
+
+    public Comment(User user, Post post, String content, Comment targetComment) {
+        this.user = user;
+        this.post = post;
+        this.content = content;
+        this.targetId = targetComment != null ? targetComment.getId() : null;
+    }
+
+    public void updateInactive() {
+        this.baseStatus = BaseStatus.INACTIVE;
+    }
+}
