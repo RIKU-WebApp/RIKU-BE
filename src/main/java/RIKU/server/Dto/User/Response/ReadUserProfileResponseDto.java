d47a0e6f52ef3ca38550db88f38cbb5ffb6c320f
arkchive <kyungminkim7@konkuk.ac.kr>
2025-02-05 16:33:05 +0900

    refactor: 유저포인트 엔티티 추가로 인한 변경

--- src/main/java/RIKU/server/Dto/User/Response/ReadUserProfileResponseDto.java
+++ src/main/java/RIKU/server/Dto/User/Response/ReadUserProfileResponseDto.java
@@ -16,7 +16,7 @@ public class ReadUserProfileResponseDto {
     private String userName;
     private String userProfileImg;
     private UserRole userRole;
-    private int points = 0;
+    private int points;
     private int attendanceCount = 0;
 
     public static ReadUserProfileResponseDto of(User user) {
@@ -25,7 +25,7 @@ public class ReadUserProfileResponseDto {
                 .userName(user.getName())
                 .userProfileImg(user.getProfileImageUrl())
                 .userRole(user.getUserRole())
-                .points(0)
+                .points(user.getTotalPoints())
                 .attendanceCount(0)
                 .build();
     }
