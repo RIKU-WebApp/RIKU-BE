f8679fdc9f8974d8423d438388d11bcf9c2c0acd
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-18 01:32:58 +0900

    feat: 게시글 러닝 취소 로직 추가

--- src/main/java/RIKU/server/Controller/Post/PostController.java
+++ src/main/java/RIKU/server/Controller/Post/PostController.java
@@ -1,17 +1,12 @@
 package RIKU.server.Controller.Post;
 
-import RIKU.server.Dto.Post.Request.CreatePostRequestDto;
 import RIKU.server.Dto.Post.Response.ReadPostListResponse;
-import RIKU.server.Dto.Post.Response.ReadPostDetailResponseDto;
 import RIKU.server.Security.AuthMember;
 import RIKU.server.Service.Post.PostService;
 import RIKU.server.Util.BaseResponse;
-import RIKU.server.Util.Exception.Validation.FieldValidationException;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -36,43 +31,48 @@ public class PostController {
         return new BaseResponse<>(posts);
     }
 
-    // 게시글 상세 조회
-    @GetMapping("/post/{postId}")
-    public BaseResponse<ReadPostDetailResponseDto> getPost(@PathVariable Long postId) {
-        ReadPostDetailResponseDto responseDto = postService.getPostDetail(postId);
-        return new BaseResponse<>(responseDto);
+//    // 게시글 상세 조회
+//    @GetMapping("/post/{postId}")
+//    public BaseResponse<ReadPostDetailResponseDto> getPost(@PathVariable Long postId) {
+//        ReadPostDetailResponseDto responseDto = postService.getPostDetail(postId);
+//        return new BaseResponse<>(responseDto);
+//
+//    }
 
-    }
+//    // 게시글 수정하기
+//    @PutMapping("/post/{postId}")
+//    public BaseResponse<Map<String, Long>> updatePost(
+//            @PathVariable Long postId,
+//            @ModelAttribute @Validated CreatePostRequestDto requestDto,
+//            BindingResult bindingResult,
+//            @AuthenticationPrincipal AuthMember authMember) {
+//
+//        // 유효성 검증 실패 시 처리
+//        if (bindingResult.hasFieldErrors()) throw new FieldValidationException(bindingResult);
+//
+//        Long updatedPostId = postService.updatePost(authMember.getId(), postId, requestDto);
+//
+//        Map<String, Long> response = new HashMap<>();
+//        response.put("postId", updatedPostId);
+//
+//        return new BaseResponse<>(response);
+//    }
 
-    // 게시글 수정하기
-    @PutMapping("/post/{postId}")
-    public BaseResponse<Map<String, Long>> updatePost(
+    @Operation(summary = "게시글 러닝 취소", description = """
+            
+            유저가 게시글을 취소합니다.(번개런은 생성자 권한, 나머지는 운영진 권한)
+            
+            """)
+    @PatchMapping("/post/{postId}")
+    public BaseResponse<Map<String, Long>> cancelPost(
+            @PathVariable String runType,
             @PathVariable Long postId,
-            @ModelAttribute @Validated CreatePostRequestDto requestDto,
-            BindingResult bindingResult,
             @AuthenticationPrincipal AuthMember authMember) {
 
-        // 유효성 검증 실패 시 처리
-        if (bindingResult.hasFieldErrors()) throw new FieldValidationException(bindingResult);
-
-        Long updatedPostId = postService.updatePost(authMember.getId(), postId, requestDto);
+        Long canceledPostId = postService.cancelPost(authMember, runType, postId);
 
         Map<String, Long> response = new HashMap<>();
-        response.put("postId", updatedPostId);
-
-        return new BaseResponse<>(response);
-    }
-
-    // 게시글 삭제하기
-    @DeleteMapping("/post/{postId}")
-    public BaseResponse<Map<String, Long>> deletePost(
-            @PathVariable Long postId,
-            @AuthenticationPrincipal AuthMember authMember) {
-
-        postService.deletePost(authMember.getId(), postId);
-
-        Map<String, Long> response = new HashMap<>();
-        response.put("postId", postId);
+        response.put("canceledPostId", canceledPostId);
 
         return new BaseResponse<>(response);
     }
--- src/main/java/RIKU/server/Service/Post/PostService.java
+++ src/main/java/RIKU/server/Service/Post/PostService.java
@@ -1,12 +1,8 @@
 package RIKU.server.Service.Post;
 
-import RIKU.server.Dto.Post.Request.CreatePostRequestDto;
 import RIKU.server.Dto.Post.Response.ReadPostListResponse;
-import RIKU.server.Dto.Post.Response.ReadCommentsResponseDto;
-import RIKU.server.Dto.Post.Response.ReadPostDetailResponseDto;
 import RIKU.server.Dto.Post.Response.ReadPostPreviewResponse;
 import RIKU.server.Entity.Base.BaseStatus;
-import RIKU.server.Entity.Board.Comment;
 import RIKU.server.Entity.Board.Post.Post;
 import RIKU.server.Entity.Board.Post.PostType;
 import RIKU.server.Entity.Board.PostStatus;
@@ -14,6 +10,7 @@ import RIKU.server.Repository.CommentRepository;
 import RIKU.server.Repository.ParticipantRepository;
 import RIKU.server.Repository.PostRepository;
 import RIKU.server.Repository.UserRepository;
+import RIKU.server.Security.AuthMember;
 import RIKU.server.Service.S3Uploader;
 import RIKU.server.Util.BaseResponseStatus;
 import RIKU.server.Util.Exception.Domain.PostException;
@@ -23,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
@@ -83,88 +79,109 @@ public class PostService {
         return ReadPostListResponse.of(todayRuns, upcomingRuns, pastRuns);
     }
 
-    // 게시글 상세 조회
-    public ReadPostDetailResponseDto getPostDetail(Long postId) {
-        // 게시글 조회
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new PostException(BaseResponseStatus.POST_NOT_FOUND));
+//    // 게시글 상세 조회
+//    public ReadPostDetailResponseDto getPostDetail(Long postId) {
+//        // 게시글 조회
+//        Post post = postRepository.findById(postId)
+//                .orElseThrow(() -> new PostException(BaseResponseStatus.POST_NOT_FOUND));
+//
+//        // 댓글 조회
+//        List<ReadCommentsResponseDto> comments = commentRepository.findByPost(post).stream()
+//                .filter(comment -> comment.getTargetId() == null) // 최상위 댓글만 조회
+//                .map(this::mapCommentToDto) // 댓글 -> DTO 변환
+//                .toList();
+//        return ReadPostDetailResponseDto.of(post, comments);
+//    }
+//
+//    // 댓글 -> DTO 변환
+//    private ReadCommentsResponseDto mapCommentToDto(Comment comment) {
+//        // 대댓글 리스트 변환
+//        List<ReadCommentsResponseDto> replies = commentRepository.findByTargetId(comment.getId()).stream()
+//                .map(this::mapCommentToDto)
+//                .collect(Collectors.toList());
+//
+//        return ReadCommentsResponseDto.of(comment, replies);
+//    }
 
-        // 댓글 조회
-        List<ReadCommentsResponseDto> comments = commentRepository.findByPost(post).stream()
-                .filter(comment -> comment.getTargetId() == null) // 최상위 댓글만 조회
-                .map(this::mapCommentToDto) // 댓글 -> DTO 변환
-                .toList();
-        return ReadPostDetailResponseDto.of(post, comments);
-    }
-
-    // 댓글 -> DTO 변환
-    private ReadCommentsResponseDto mapCommentToDto(Comment comment) {
-        // 대댓글 리스트 변환
-        List<ReadCommentsResponseDto> replies = commentRepository.findByTargetId(comment.getId()).stream()
-                .map(this::mapCommentToDto)
-                .collect(Collectors.toList());
-
-        return ReadCommentsResponseDto.of(comment, replies);
-    }
-
-    // 게시글 수정하기
-    @Transactional
-    public Long updatePost(Long userId, Long postId, CreatePostRequestDto requestDto) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new PostException(BaseResponseStatus.POST_NOT_FOUND));
-
-        // 게시글 작성자 검증
-        if (!post.getCreatedBy().getId().equals(userId)) {
-            throw new UserException(BaseResponseStatus.UNAUTHORIZED_USER);
-        }
-
-        // 유효한 집합 날짜인지 확인
-        LocalDateTime now = LocalDateTime.now();
-        if (requestDto.getDate().isBefore(now)) {
-            throw new PostException(BaseResponseStatus.INVALID_DATE_AND_TIME);
-        }
-
-        String postImageUrl = post.getPostImageUrl();
-
-        // 이미지 처리
-        if (requestDto.getPostImage() != null) {
-            if (!requestDto.getPostImage().isEmpty()) {
-                // 새로운 이미지 업로드
-                try {
-                    log.debug("Received post image: {}", requestDto.getPostImage().getOriginalFilename());
-                    postImageUrl = s3Uploader.upload(requestDto.getPostImage(), "postImg");
-                    log.debug("post image uploaded: {}", postImageUrl);
-
-                } catch (IOException e) {
-                    log.error("File upload failed: {}", requestDto.getPostImage().getOriginalFilename(), e);
-                    throw new PostException(BaseResponseStatus.POST_IMAGE_UPLOAD_FAILED);
-                }
-            }
-        } else {
-            // 이미지를 없애는 경우
-            postImageUrl = null;
-
-        }
-
-        post.updatePost(requestDto.getTitle(), requestDto.getLocation(), requestDto.getDate(), requestDto.getContent(), postImageUrl);
-
-        return post.getId();
-    }
+//    // 게시글 수정하기
+//    @Transactional
+//    public Long updatePost(Long userId, Long postId, CreatePostRequestDto requestDto) {
+//        Post post = postRepository.findById(postId)
+//                .orElseThrow(() -> new PostException(BaseResponseStatus.POST_NOT_FOUND));
+//
+//        // 게시글 작성자 검증
+//        if (!post.getCreatedBy().getId().equals(userId)) {
+//            throw new UserException(BaseResponseStatus.UNAUTHORIZED_USER);
+//        }
+//
+//        // 유효한 집합 날짜인지 확인
+//        LocalDateTime now = LocalDateTime.now();
+//        if (requestDto.getDate().isBefore(now)) {
+//            throw new PostException(BaseResponseStatus.INVALID_DATE_AND_TIME);
+//        }
+//
+//        String postImageUrl = post.getPostImageUrl();
+//
+//        // 이미지 처리
+//        if (requestDto.getPostImage() != null) {
+//            if (!requestDto.getPostImage().isEmpty()) {
+//                // 새로운 이미지 업로드
+//                try {
+//                    log.debug("Received post image: {}", requestDto.getPostImage().getOriginalFilename());
+//                    postImageUrl = s3Uploader.upload(requestDto.getPostImage(), "postImg");
+//                    log.debug("post image uploaded: {}", postImageUrl);
+//
+//                } catch (IOException e) {
+//                    log.error("File upload failed: {}", requestDto.getPostImage().getOriginalFilename(), e);
+//                    throw new PostException(BaseResponseStatus.POST_IMAGE_UPLOAD_FAILED);
+//                }
+//            }
+//        } else {
+//            // 이미지를 없애는 경우
+//            postImageUrl = null;
+//
+//        }
+//
+//        post.updatePost(requestDto.getTitle(), requestDto.getLocation(), requestDto.getDate(), requestDto.getContent(), postImageUrl);
+//
+//        return post.getId();
+//    }
 
 
 
     // 게시글 삭제하기
     @Transactional
-    public void deletePost(Long userId, Long postId) {
+    public Long cancelPost(AuthMember authMember, String runType, Long postId) {
+        // 1. PostType 검증
+        PostType postType = validatePostType(runType);
+
+        // 2. 게시글 조회
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new PostException(BaseResponseStatus.POST_NOT_FOUND));
 
-        // 게시글 작성자 검증
-        if (!post.getCreatedBy().getId().equals(userId)) {
-            throw new UserException(BaseResponseStatus.UNAUTHORIZED_USER);
+        // 3. 게시글 작성자 검증
+        if (postType == PostType.FLASH) {
+            // 번개런이면 생성자 권한으로 취소 가능
+            if (!post.getPostCreator().getId().equals(authMember.getId())) {
+                throw new UserException(BaseResponseStatus.UNAUTHORIZED_USER);
+            }
+        } else {
+            // 번개런이 아닌 경우 운영진만 취소 가능
+            if (!authMember.isAdmin()) {
+                throw new UserException(BaseResponseStatus.UNAUTHORIZED_USER);
+            }
         }
 
         post.updateStatus(PostStatus.CANCELED);
+        return post.getId();
+    }
+
+    private PostType validatePostType(String runType) {
+        try {
+            return PostType.valueOf(runType.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 runType 입니다: " + runType);
+        }
     }
 
     private boolean isToday(LocalDateTime postDate, LocalDateTime now) {
