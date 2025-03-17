fd3dca2de372c1fc8002cf8163e456ca787f048c
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-18 02:52:03 +0900

    refactor: UserRole @Getter 추가, INACTIVE 삭제

--- src/main/java/RIKU/server/Entity/User/UserRole.java
+++ src/main/java/RIKU/server/Entity/User/UserRole.java
@@ -1,10 +1,12 @@
 package RIKU.server.Entity.User;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
     NEW_MEMBER("NEW_MEMBER"),   // 신입 부원
     MEMBER("MEMBER"),     // 일반 부원 (번개런 생성 가능)
-    ADMIN("ADMIN"),      // 운영진 및 페이서 (모든 권한 부여)
-    INACTIVE("INACTIVE");    // 비활성화 사용자
+    ADMIN("ADMIN");     // 운영진 및 페이서 (모든 권한 부여)
 
     UserRole(String value) {
         this.value = value;
@@ -14,12 +16,4 @@ public enum UserRole {
     private final String PREFIX = "ROLE_";
     private final String value;
     private final String role;
-
-    public String getValue() {
-        return value;
-    }
-
-    public String getRole() {
-        return role;
-    }
 }
