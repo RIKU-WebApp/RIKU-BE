047d85365300983d5c6a5b777ebf2bb853e2f24a
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-18 10:46:10 +0900

    refactor: UpdateUserRole @NoArgsConstructor 추가

--- src/main/java/RIKU/server/Dto/User/Request/UpdateUserRoleRequest.java
+++ src/main/java/RIKU/server/Dto/User/Request/UpdateUserRoleRequest.java
@@ -1,22 +1,13 @@
 package RIKU.server.Dto.User.Request;
 
-import RIKU.server.Entity.User.User;
-import RIKU.server.Entity.User.UserRole;
-import lombok.Builder;
-import lombok.Getter;
+import RIKU.server.Entity.User.UserRole;import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
+@NoArgsConstructor
 public class UpdateUserRoleRequest {
 
     private String studentId;
 
     private UserRole userRole;
-
-    public static UpdateUserRoleRequest of(User user) {
-        return UpdateUserRoleRequest.builder()
-                .studentId(user.getStudentId())
-                .userRole(user.getUserRole())
-                .build();
-    }
 }
