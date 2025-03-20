a7fbc592eb353bde50ac6a6d48e2d6ba33b01eeb
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-21 01:59:56 +0900

    fix: 출석 종료 http method 변경

--- src/main/java/RIKU/server/Controller/ParticipantController.java
+++ src/main/java/RIKU/server/Controller/ParticipantController.java
@@ -75,7 +75,7 @@ public class ParticipantController {
             생성자가 러닝에 출석을 종료합니다.(번개런은 생성자 권한, 나머지는 운영진 권한)
             
             """)
-    @PutMapping("/close")
+    @PatchMapping("/close")
     public BaseResponse<Map<String, Object>> closeRun(
             @PathVariable String runType,
             @PathVariable Long postId,
