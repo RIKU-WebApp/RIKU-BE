04e4860fd384feea7376a2335bfaee3658803dd9
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-20 23:14:01 +0900

    fix: user 테이블명 수정

--- src/main/java/RIKU/server/Entity/User/User.java
+++ src/main/java/RIKU/server/Entity/User/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
