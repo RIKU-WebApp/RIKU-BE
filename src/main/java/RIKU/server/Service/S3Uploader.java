cec8470e30bb491274e7d81db314ab676641cdee
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-31 16:14:00 +0900

    feat: s3 이미지 삭제 로직 추가

--- src/main/java/RIKU/server/Service/S3Uploader.java
+++ src/main/java/RIKU/server/Service/S3Uploader.java
@@ -1,6 +1,5 @@
 package RIKU.server.Service;
 
-import RIKU.server.Util.AllowedImageFileExtensions;
 import RIKU.server.Util.Exception.Domain.CustomException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -11,11 +10,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Optional;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.UUID;
 
 import static RIKU.server.Util.BaseResponseStatus.MULTIPARTFILE_CONVERT_FAIL_IN_MEMORY;
@@ -24,19 +22,12 @@ import static RIKU.server.Util.BaseResponseStatus.MULTIPARTFILE_CONVERT_FAIL_IN_
 @RequiredArgsConstructor
 @Service
 public class S3Uploader {
+
     private final AmazonS3 amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    //MultipartFile을 전달 받아 File로 전환 후 S3 업로드
-//    public String uploadFile(MultipartFile multipartFile, String dirName) throws IOException{// dirName의 디렉토리가 S3 Bucket 내부에 생성됨
-//
-//        File uploadFile = convert(multipartFile).orElseThrow(()-> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
-//        //System.out.p
-//        // print("error: multipart file input. cant control");
-//        return upload(uploadFile,dirName);
-//    }
 
     // File에 저장하지 않고 Memory에서 변환 시행
     public String upload(MultipartFile file, String dirName) throws IOException {
@@ -65,75 +56,31 @@ public class S3Uploader {
         return dirName + "/" + UUID.randomUUID().toString() + fileExtension;
     }
 
-    //    public String upload(File uploadFile, String dirName){
-//        String fileName = dirName+"/"+uploadFile.getName();
-//        String uploadImageUrl = putS3(uploadFile,fileName);
-//
-//        removeNewFile(uploadFile);// convert()함수로 인해서 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
-//        return uploadImageUrl;
-//    }
-    // 업로드하기
-    private String putS3(File uploadFile, String fileName){
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile));
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+    // 이미지 지우기
+    public void deleteFileByUrl(String fileUrl) {
+        // 예시: https://bucket.s3.amazonaws.com/dirName/profile.xxx
+        // S3 URL 형식에 따라 객체 키를 추출하는 로직
+        try {
+            URL url = new URL(fileUrl);
+            String path = url.getPath();        // 예시: /dirName/profile.xxx
+            String key = path.startsWith("/") ? path.substring(1) : path;       // path에서 key 파싱
+
+            if (amazonS3Client.doesObjectExist(bucket, key)) {
+                amazonS3Client.deleteObject(bucket, key);
+                log.info("S3에서 파일 삭제됨 : {}", key);
+            } else {
+                log.warn("삭제할 파일이 존재하지 않음 : {}", key);
+            }
+        } catch (MalformedURLException e) {
+            log.error("URL 파싱 오류 : {}", fileUrl, e);
+        }
     }
-    private String putS3(String fileName,InputStream uploadFile, ObjectMetadata metadata) {
+
+    // 업로드하기
+    private String putS3(String fileName, InputStream uploadFile, ObjectMetadata metadata) {
         log.info("Uploading to S3 bucket: {}, file: {}", bucket, fileName);
         amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile,metadata));
         return amazonS3Client.getUrl(bucket, fileName).toString();
-    }
-    // 이미지 지우기
-    private void removeNewFile(File targetFile) {
-        if (targetFile.delete()) {
-            log.info("File delete success");
-            return;
-        }
-        log.info("File delete fail");
-    }
-    private Optional<File> convert(MultipartFile file) throws  IOException {
-        File convertFile = new File(file.getOriginalFilename()); // 업로드한 파일의 이름
-        if(convertFile.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
-        }
-        return Optional.empty();
-    }
-
-    // MultipartFile이 지원하는 이미지 파일 형식인지 검증
-    public boolean isFileImage(MultipartFile file) {
-        String fileName = file.getOriginalFilename();
-        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
-
-        log.info("extension : {}", extension);
-
-        return AllowedImageFileExtensions.contains(extension);
-    }
-
-
-    private String getFileExtension(String[] base64Components) {
-        String filePrefix = base64Components[0].split(";")[0].split(":")[1];
-        return switch (filePrefix) {
-            case "image/jpeg" -> ".jpeg";
-            case "image/png" -> ".png";
-            case "image/jpg" -> ".jpg";
-            case "image/gif" -> ".gif";
-            case "image/webp" -> ".webp";
-            case "image/svg+xml" -> ".svg";
-            case "image/bmp" -> ".bmp";
-            case "image/tif" -> ".tif";
-            case "image/tiff" -> ".tiff";
-            case "image/heic" -> ".heic";
-            case "application/pdf" -> ".pdf";
-            case "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document" -> ".docx";
-            case "application/vnd.hwp" -> ".hwp";
-            case "text/plain" -> ".txt";
-            default -> {
-                log.info("Unsupported file prefix: " + filePrefix);
-                yield ".bin"; // 기본 확장자
-            }
-        };
     }
 
     private String getFileExtension(String fileName){
