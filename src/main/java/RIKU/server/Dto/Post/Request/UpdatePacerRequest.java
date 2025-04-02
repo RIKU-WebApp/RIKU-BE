2035a761e8f31ba4542f0e35aa3e83adfc161672
arkchive <kyungminkim7@konkuk.ac.kr>
2025-04-02 15:57:44 +0900

    fix: 페이서 정보 수정 dto @setter 추가

--- src/main/java/RIKU/server/Dto/Post/Request/UpdatePacerRequest.java
+++ src/main/java/RIKU/server/Dto/Post/Request/UpdatePacerRequest.java
@@ -6,8 +6,10 @@ import RIKU.server.Entity.User.User;
 import jakarta.annotation.Nullable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UpdatePacerRequest {
 
