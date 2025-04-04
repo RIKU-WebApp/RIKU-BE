77d6cc1952d69f0ea04ea1aed3a16ecc6957963a
arkchive <kyungminkim7@konkuk.ac.kr>
2025-04-05 02:35:40 +0900

    refactor: 일정 조회 시, response 게시글 타입 추가

--- src/main/java/RIKU/server/Dto/Calendar/Response/ReadDailyScheduleResponse.java
+++ src/main/java/RIKU/server/Dto/Calendar/Response/ReadDailyScheduleResponse.java
@@ -10,14 +10,17 @@ public class ReadDailyScheduleResponse {
 
     private Long postId;
 
+    private String postType;
+
     private String title;
 
     private LocalDateTime date;
 
     private String location;
 
-    private ReadDailyScheduleResponse(Long postId, String title, LocalDateTime date, String location) {
+    private ReadDailyScheduleResponse(Long postId, String postType, String title, LocalDateTime date, String location) {
         this.postId = postId;
+        this.postType = postType;
         this.title = title;
         this.date = date;
         this.location = location;
@@ -26,6 +29,7 @@ public class ReadDailyScheduleResponse {
     public static ReadDailyScheduleResponse of(Post post) {
         return new ReadDailyScheduleResponse(
                 post.getId(),
+                post.getPostType().toString(),
                 post.getTitle(),
                 post.getDate(),
                 post.getLocation()
