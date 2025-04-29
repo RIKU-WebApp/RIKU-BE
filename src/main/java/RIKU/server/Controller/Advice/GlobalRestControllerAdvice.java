a13168e9314a13420d40b759491f1a9b3c836789
arkchive <kyungminkim7@konkuk.ac.kr>
2025-04-29 23:04:52 +0900

    feat: CustomJwtException 핸들러 추가

--- src/main/java/RIKU/server/Controller/Advice/GlobalRestControllerAdvice.java
+++ src/main/java/RIKU/server/Controller/Advice/GlobalRestControllerAdvice.java
@@ -1,9 +1,12 @@
 package RIKU.server.Controller.Advice;
 
 import RIKU.server.Util.BaseResponseStatus;
+import RIKU.server.Util.Exception.CustomJwtException;
 import RIKU.server.Util.Exception.Domain.CustomException;
 import RIKU.server.Util.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
@@ -21,5 +24,13 @@ public class GlobalRestControllerAdvice {
     public BaseResponse<Object> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
         log.error("[MaxUploadSizeExceededException] 요청 크기 초과: {}", e.getMessage());
         return new BaseResponse<>(BaseResponseStatus.REQUEST_SIZE_EXCEEDED);
+    }
+
+    @ExceptionHandler(CustomJwtException.class)
+    public ResponseEntity<BaseResponse<Object>> handleCustomJwtException(CustomJwtException e) {
+        log.warn("[CustomJwtException] {}", e.getStatus().getResponseMessage());
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)  // 항상 401
+                .body(new BaseResponse<>(e.getStatus()));
     }
 }
