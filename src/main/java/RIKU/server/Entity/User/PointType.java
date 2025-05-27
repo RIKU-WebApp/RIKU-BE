aca48beb4b9cc08c8675e095418bdb7f21398d2c
arkchive <kyungminkim7@konkuk.ac.kr>
2025-05-27 12:47:49 +0900

    refactor: 스웨거 커스터마이징

--- src/main/java/RIKU/server/Entity/User/PointType.java
+++ src/main/java/RIKU/server/Entity/User/PointType.java
@@ -1,11 +1,27 @@
 package RIKU.server.Entity.User;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "포인트 타입")
 public enum PointType {
-    ADD_REGULAR_JOIN,   // 정규런 참여(+10)
-    ADD_FLASH_CREATE,   // 번개런 생성(+7)
-    ADD_FLASH_JOIN,     // 번개런 참여(+5)
-    ADD_TRAINING_JOIN,  // 훈련 참여(+8)
-    ADD_EVENT_JOIN,     // 행사 참여(+8)
-    ADD_ATTENDANCE,     // 오늘의 출석(+1)
-    REMOVE   // 차감
+    @Schema(description = "정규런 참여 (+10)")
+    ADD_REGULAR_JOIN,
+
+    @Schema(description = "번개런 생성 (+7)")
+    ADD_FLASH_CREATE,
+
+    @Schema(description = "번개런 참여 (+5)")
+    ADD_FLASH_JOIN,
+
+    @Schema(description = "훈련 참여 (+8)")
+    ADD_TRAINING_JOIN,
+
+    @Schema(description = "행사 참여 (+8)")
+    ADD_EVENT_JOIN,
+
+    @Schema(description = "출석 (+1)")
+    ADD_ATTENDANCE,
+
+    @Schema(description = "포인트 차감")
+    REMOVE
 }
